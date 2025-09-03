a3eeb05c8a4a8e08b55fc31ef4189532c53b1382
Khaled Ahmed <khhaledahmaad@gmail.com>
2025-09-03 23:34:31 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with io.open(
 
 setup(
     name="my_etl_package",
-    version="0.0.9",
+    version="0.1.1",
     description="A package for ETL pipeline operations",
     long_description=long_description,
     long_description_content_type="text/markdown",
